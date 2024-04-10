baee815026f661adcdcc435540ae20314620a188
Vigneshwaran V <vigneshwaranvengadesh@gmail.com>
2024-04-10 17:57:28 +0530

    port added

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3001,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000'
+        target: 'http://13.200.152.131:5000'
       }
     }
   }
