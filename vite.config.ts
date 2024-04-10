79a0edc9f3d1ee5b5b2e56174a22c67ce339face
Vigneshwaran V <vigneshwaranvengadesh@gmail.com>
2024-04-10 15:06:38 +0530

    port added

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    host:true,
+    port: 3001,
     proxy: {
       '/api': {
         target: 'http://localhost:5000'
